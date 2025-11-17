4d13fee685ac15a6e8bcf4b4c964af17d21404b1
danialrahimian <onedaniyel@gmail.com>
2025-11-17 17:21:51 +0330

    fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,40 +1,31 @@
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
 } from "./ui/navigation-menu";
 import { ModeToggle } from "./mode-toggle";
 import { Link } from "react-router";
 import { navbarItems } from "../data/navbarItems";
 export default function Navbar() {
   return (
-    <NavigationMenu className="w-full h-14 flex justify-center items-center backdrop-blur-2xl">
-      <NavigationMenuList>
-        {navbarItems.map((navbarItem) => {
-          return (
-            <NavigationMenuItem key={navbarItem.id}>
-              <NavigationMenuTrigger>
-                {navbarItem.navbarCategory}
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                {navbarItem.links.map((link) => {
-                  return (
-                    <NavigationMenuLink asChild key={link.id}>
-                      <Link to={link.href}>{link.name}</Link>
-                    </NavigationMenuLink>
-                  );
-                })}
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          );
-        })}
-        <NavigationMenuItem>
-          <ModeToggle />
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <div className="w-full h-14 flex justify-center items-center backdrop-blur-2xl">
+      <NavigationMenu>
+        <NavigationMenuList>
+          {navbarItems.map((navbarItem) => {
+            return (
+              <NavigationMenuItem key={navbarItem.id}>
+                <NavigationMenuLink asChild>
+                  <Link to={navbarItem.href}>{navbarItem.name}</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            );
+          })}
+          <NavigationMenuItem>
+            <ModeToggle />
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
   );
 }
--- src/data/navbarItems.ts
+++ src/data/navbarItems.ts
@@ -1,51 +1,35 @@
 export const navbarItems = [
   {
+    id: 0,
+    name: "Home",
+    href: "/",
+  },
+  {
     id: 1,
-    navbarCategory: "Crypto Currency",
-    links: [
-      {
-        id: 1,
-        name: "Coins",
-        href: "/coins",
-      },
-      {
-        id: 2,
-        name: "Exchanges",
-        href: "/exchanges",
-      },
-    ],
+    name: "Coins",
+    href: "/coins",
   },
   {
     id: 2,
-    navbarCategory: "NFT",
-    links: [
-      {
-        id: 3,
-        name: "NFTs",
-        href: "/nfts",
-      },
-    ],
+    name: "Exchanges",
+    href: "/exchanges",
   },
+
   {
     id: 3,
-    navbarCategory: "Asset Platforms",
-    links: [
-      {
-        id: 4,
-        name: "Asset Platforms",
-        href: "/asset-platforms",
-      },
-    ],
+    name: "NFTs",
+    href: "/nfts",
   },
+
   {
     id: 4,
-    navbarCategory: "Trends",
-    links: [
-      {
-        id: 5,
-        name: "Trends",
-        href: "/trends",
-      },
-    ],
+    name: "Asset Platforms",
+    href: "/asset-platforms",
+  },
+
+  {
+    id: 5,
+    name: "Trends",
+    href: "/trends",
   },
 ];
